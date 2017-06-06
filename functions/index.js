76fa450321760e2f0bf103417f03cf22248864c6
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-06 12:25:55 +0800

    fixes wrong filename

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = require('./service_account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
